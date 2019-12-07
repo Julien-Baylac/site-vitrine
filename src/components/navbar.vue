269cba68b1b4c0082b3fe4e84224dc037b1ea95f
oOw <julien.baylac@protonmail.com>
2019-12-07 10:10:32 +0100

    initial commit

---
+++ src/components/navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>navbar</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
